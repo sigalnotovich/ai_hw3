2e1a6f79fbbe3076e4dd14a3dce8b41c2e8063d3
sigali <sigals.notov@gmail.com>
2021-01-16 19:05:09 +0200

    pandas done. pandas too slow:\
    next step:
    read file with numpy instead of pandas

--- ID3.py
+++ ID3.py
@@ -5,8 +5,8 @@ eps = np.finfo(float).eps
 from numpy import log2 as log
 
 
-#todo: do i need initial entropy in this way?
-def initial_entropy_in_database(df):
+
+def get_entropy_before_division(df):
     entropy_node = 0  # Initialize Entropy
     values = df.diagnosis.unique()  # Unique objects - 'B', 'M'
     for value in values:
@@ -18,7 +18,7 @@ def initial_entropy_in_database(df):
 
 #takes an attribute,looks at its values and devide into borders ,as we saw in the lecture,
 #then add the sum over i to k of |Ei|/|E| * H(Ei) to the entropy_list
-def find_entropy_attribute(df, attribute):
+def find_entropy_for_different_divisions_for_attribute(df, attribute):
     #B_or_M = df.diagnosis.unique()  # This gives all 'B' and 'M'
     #print(B_or_M)
     # variables = df[attribute].unique()  # This gives all the values in the column of the attribute ,if there is a
@@ -42,23 +42,9 @@ def find_entropy_attribute(df, attribute):
 
         relation_of_chosen_from_all = len(df[attribute][df[attribute] < border])/len(df)
         new_entropy = relation_of_chosen_from_all * h_first_sun + relation_of_chosen_from_all * h_second_sun
-        entropy_list.append(new_entropy)
+        entropy_list.append((new_entropy, attribute, border))
 
     return entropy_list
-
-    # entropy_attribute = 0
-    # for variable in variables: #variable should be a threshold for which after him you are B and after you are M
-    #     entropy_each_feature = 0
-    #     for target_variable in B_or_M:
-    #         num = len(df[attribute][df[attribute] == variable][df.diagnosis == target_variable])  # numerator - for ex. how much from this attribute that are B
-    #         den = len(df[attribute][df[attribute] == variable])  # denominator #for how much of this attribute
-    #         fraction = num / (den + eps)  # pi
-    #         entropy_each_feature += -fraction * log(fraction + eps)  # This calculates entropy for one feature like 'radius_mean'
-    #
-    #     fraction2 = den / len(df)
-    #     entropy_attribute += -fraction2 * entropy_each_feature  # Sums up all the entropy ETaste
-    #
-    # return (abs(entropy_attribute))
 
 
 def dynamic_division(df, attribute):
@@ -71,65 +57,135 @@ def dynamic_division(df, attribute):
     return borders(variables)
 
 #get the entropy for all the attributes when each attribute devided to borders as we saw in the lecture
-def get_entropy_list(df):
-    entropy_list = []
+def get_division_options_entropy_list(df):
+    division_options_entropy_list = []
     for attribute in df.keys()[1:]:
         # print(attribute)
-        received_entropy_list = find_entropy_attribute(df, attribute)
-        entropy_list += received_entropy_list
-    return entropy_list
+        received_entropy_list_for_an_attribute = find_entropy_for_different_divisions_for_attribute(df, attribute)
+        division_options_entropy_list += received_entropy_list_for_an_attribute
+    return division_options_entropy_list
+
+
+
+def MAX_IG(df):
+    entropy_before_division = get_entropy_before_division(df)
+    division_options_entropy_list = get_division_options_entropy_list(df)
+    IG = [(entropy_before_division - entropy, attribute, border) for entropy, attribute, border in division_options_entropy_list]
+    best_entropy_dif, best_attribute_name,  best_attribute_limit = max(IG,key=lambda item: item[0])  #for ex. (0.9457760422765744, 'fractal_dimension_mean', 0.050245)
+    return best_entropy_dif, best_attribute_name,  best_attribute_limit  #for ex. (0.9457760422765744, 'fractal_dimension_mean', 0.050245)?
+
+
+def get_subtable_under_limit(df, attribute, limit):
+    return df[df[attribute] < limit].reset_index(drop=True)
+
+
+def get_subtable_above_equal_limit(df, attribute, limit):
+    return df[df[attribute] >= limit].reset_index(drop=True)
+
+
+
+def buildTree(df, tree=None):
+    Class = df.keys()[0]  # diagnosis
+
+    # Here we build our decision tree
+
+    # Get attribute with maximum information gain
+    best_entropy_dif, best_attribute_name, best_attribute_limit = MAX_IG(df)
+
+    print(best_attribute_name,best_attribute_limit)
+
+    # Create an empty dictionary to create tree
+    if tree is None:
+        tree = {}
+        tree[best_attribute_name] = {}
+
+    print("tree")
+    print(tree)
+
+    subtable_under_limit = get_subtable_under_limit(df, best_attribute_name, best_attribute_limit)
+    subtable_under_limit.to_csv(r'C:\My Stuff\studies\2021a\AI\hw3\check\under_limit.csv')
+    print("subtable_under_limit")
+    print(subtable_under_limit)
+
+    subtable_above_equal_limit = get_subtable_above_equal_limit(df, best_attribute_name, best_attribute_limit)
+    subtable_above_equal_limit.to_csv(r'C:\My Stuff\studies\2021a\AI\hw3\check\above_equal_limit.csv')
+    print("subtable_above_equal_limit")
+    print(subtable_above_equal_limit)
+
+    # clValue, counts = np.unique(subtable_under_limit, return_counts=True)
+    # print("clValue" , clValue)
+    # print("counts",counts)
+
+    #clValue, counts = np.unique(subtable_above_equal_limit, return_counts=True)
+
+##   if len(subtable_above_equal_limit['diagnosis'].unique().tolist()) == 1:
+##       tree[best_attribute_name][best_attribute_limit] =
+    # if len(counts) == 1:  # Checking purity of subset
+    #     tree[best_attribute_name][best_attribute_limit] = clValue[0]
+    # else:
+    #     tree[best_attribute_name][best_attribute_limit] = buildTree(subtable_above_equal_limit)  # Calling the function recursively
+
+    return tree
+
+def getMajorityClass(df):
+    list_of_B_and_M = df['diagnosis'].tolist()
+    number_of_B = list_of_B_and_M.count('B')
+    number_of_M = list_of_B_and_M.count('M')
+    if number_of_B > number_of_M:
+        return 'B'
+    else:
+        return 'M'
+
+
+def ID3(df,node):
+
+    majority_class = getMajorityClass(df)
+
+    return TDIDT(df,majority_class,MAX_IG,node)
+
+
+def TDIDT(df, majority_class ,MAX_IG,node):  #TDIDT(E, F, Default, SelectFeature)
+    if df.empty == True:
+        node.classification = majority_class
+        return None
+
+    majority_class = getMajorityClass(df)
+
+    B_M_or_both = df['diagnosis'].unique().tolist()
+    if len(B_M_or_both) == 1 : #only B or only M
+        classification = B_M_or_both[0]
+        node.classification = classification
+        return None
+
+    best_entropy_dif, best_attribute_name, best_attribute_limit = MAX_IG(df)
+    node.partition_feature_and_limit = (best_attribute_name, best_attribute_limit)
+    subtable_under_limit = get_subtable_under_limit(df, best_attribute_name, best_attribute_limit)
+    node.left = Node()
+    TDIDT(subtable_under_limit, majority_class ,MAX_IG,node.left)
+    subtable_above_equal_limit = get_subtable_above_equal_limit(df, best_attribute_name, best_attribute_limit)
+    node.right = Node()
+    TDIDT(subtable_above_equal_limit, majority_class ,MAX_IG,node.right)
+
+class Node:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.partition_feature_and_limit = None
+        self.classification = None
+
+
+
+
+
+
+
 
 
 df = pd.read_csv("train.csv", header=0)
-initial_entropy = initial_entropy_in_database(df)
-entropy_list = get_entropy_list(df)
-print(entropy_list)
-IG = []
-IG = [initial_entropy - e for e in entropy_list]
-# for e in entropy_list:
-#     difference_in_entropy = initial_entropy - e
-#     IG.append(difference_in_entropy)
-print(IG)
-#best_parameter = df.keys()[:-1][np.argmax(IG)]
-#print(best_parameter)
+node = Node()
+ID3(df,node)
+print(node)
+#res = get_subtable(df,'perimeter_mean',54.09)
+#print(res)
 
-
-
-#todo: use the entropy_list to choose the best attribute to use.
-#reminder: now the entropy list is for every attribute i parsed on the boards and saw the entropy
-#now i need to add the name of feture and the limit(where we cut to both of the group) to the list
-#and then return the best feture and limit
-
-# print(df)
-
-
-#
-#
-# print(data)
-#
-#
-#
-#
-# def TDIDT(examples_set, features, default, selectFeature):
-#     pass
-#     if examples_set.isEmpty() :
-#         return (None, {}, default)
-#
-#     c = MajorityClass(examples_set)
-#
-#     #if all the objects in E are from the same clasification or the set of fetures is empty
-#         #return (None, {}, default)
-#
-#     selected_feature = SelectFeature(features,examples_set)
-#
-#     features = features - selected_feature #check how to do - in groups
-#
-#     Subtrees =
-#
-#
-# def MajorityClass(examples_set):
-#     pass
-#
-#
-#
-# def entropy(node_set,):
+buildTree(df)
